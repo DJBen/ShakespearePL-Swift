0ecd5bd8278380e103e216fb5295994f72ef26b5
Sihao Lu <lsh32768@gmail.com>
2016-01-20 08:29:36 +0800

    Initial Commit

---
+++ ShakespeareKit/ShakespeareKit.h
@@ -0,0 +1,19 @@
+//
+//  ShakespeareKit.h
+//  ShakespeareKit
+//
+//  Created by Sihao Lu on 1/20/16.
+//  Copyright © 2016 Sihao Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShakespeareKit.
+FOUNDATION_EXPORT double ShakespeareKitVersionNumber;
+
+//! Project version string for ShakespeareKit.
+FOUNDATION_EXPORT const unsigned char ShakespeareKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShakespeareKit/PublicHeader.h>
+
+
